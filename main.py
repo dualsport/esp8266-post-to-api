693e054ca24685967129886b4255aa78e6b6892f
Conrad Eggan <conrade@redcatmfg.com>
2020-01-23 19:58:41 -0600

    def main as function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import network
 import urequests
 import ujson
 import urandom
+import time
+
 
 LED_PIN = 2  #D4
 LED2_PIN = 16  #D0
@@ -11,6 +13,33 @@ led = machine.Pin(LED_PIN, machine.Pin.OUT)
 led2 = machine.Pin(LED2_PIN, machine.Pin.OUT)
 button = machine.Pin(BUUTON_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
 
+
+def wifi_connect(wlan, secrets):
+    while not wlan.isconnected():
+        print('\nConnecting to WiFi...')
+        with open('secrets.json') as f:
+            secrets = ujson.loads(f.read())
+        # List of allowed access points
+        acc_pts = secrets['access_points']
+        
+        # Scan for available access points
+        wifis = wlan.scan()
+        # sort strongest to weakest signal
+        wifis.sort(key=lambda x: x[3], reverse=True)
+        
+        # Loop through available access points
+        for wifi in wifis:
+            ssid = wifi[0].decode('utf-8')
+            # Is it in list of allowed access points?
+            if ssid in acc_pts:
+                wlan.connect(ssid, acc_pts[ssid])
+                time.sleep(5.0)
+                if wlan.isconnected():
+                    print('Connected to ' + ssid)
+                    print('Network config:', wlan.ifconfig())
+                    return 1
+                else:
+                    continue
 
 
 def api_post(tag, value):
@@ -40,38 +69,31 @@ def randint(min, max):
     return val
 
 
+def main():
+    print('\n\n')
+    with open('secrets.json') as f:
+        secrets = ujson.loads(f.read())
+        
+    # Turn off access point
+    ap = network.WLAN(network.AP_IF)
+    ap.active(False)
 
-print('\n\n')
-with open('secrets.json') as f:
-    secrets = ujson.loads(f.read())
-    
-# Turn off access point
-ap = network.WLAN(network.AP_IF)
-ap.active(False)
+    # Setup WiFi connection.
+    wlan = network.WLAN(network.STA_IF)
 
-# Setup WiFi connection.
-wlan = network.WLAN(network.STA_IF)
-if not wlan.isconnected():
-    wifis = wlan.scan()
-    for wifi in wifis:
-        print(wifi)
-    print('\nConnecting to WiFi.')
-    wlan.active(True)
-    wlan.connect(secrets['ssid'], secrets['pass'])
-    while not wlan.isconnected():
-        machine.idle()
-print('Connected to ' + secrets['ssid'])
-print('Network config:', wlan.ifconfig())
-
-while True:
-    value = randint(1, 1000)
-    if not button.value():
-        led.on()
-        led2.off()
-        p = api_post('test_integer', value)
-        led2.on()
-        if p == 1:
-            led.off()
+    print('Ready...')
+    while True:
+        value = randint(1, 1000)
+        if not button.value():
+            if not wlan.isconnected():
+                wifi_connect(wlan, secrets)
+            led.on()
+            led2.off()
+            p = api_post('test_integer', value)
+            led2.on()
+            if p == 1:
+                led.off()
             
-
+if __name__ == "__main__":
+    main()
 
